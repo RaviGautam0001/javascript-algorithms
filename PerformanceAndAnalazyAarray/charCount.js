efab4bb4c3d7c69a9d02c337da546212ad95e6a9
Ravi Kant <ravi.kant@magicsw.com>
2019-02-28 22:50:13 -0800

    charater count with new for loop

--- PerformanceAndAnalazyAarray/charCount.js
+++ PerformanceAndAnalazyAarray/charCount.js
@@ -1,13 +1,20 @@
 function charCount (str) {
+    //make object to return at end
     var result = {};
     for(var i = 0; i < str.length; i++){
         var char = str[i].toLowerCase();
+        // If the char is a number/letter AND is a key in object, 
+        // Add one to count
         if(result[char] > 0){
             result[char]++;
         }else{
+            // If the char is a number/letter AND not in object, 
+            // Add it to the object and set value
             result[char] = 1;
         }
     }
+    //If character is something else [space, period, etc]. don't do anything
+    //return an object at end
     return result;
 }
 
---
+++ PerformanceAndAnalazyAarray/charCountForLoopD.js
@@ -0,0 +1,15 @@
+function charFoorLoopD(str){
+    var obj = {};
+    for(var char of str){
+        char = char.toLowerCase();
+        if(/[a-z0-9]/.test(char)){
+          if(obj[char] > 0){
+              obj[char]++;
+          }else{
+              obj[char] = 1;
+          }
+        }
+    }
+    return obj;
+}
+console.log(charFoorLoopD("Hello hi threre 1254"));
