ddcc43ca54bceff545a9c07034884f354360d8ae
Ravi Kant <ravi.kant@magicsw.com>
2019-03-04 03:10:57 -0800

    remove console.log()

--- SearchingAlogrithm/NaiveSearch.js
+++ SearchingAlogrithm/NaiveSearch.js
@@ -3,11 +3,9 @@ function naiveSearch (long, short) {
     for(var i = 0; i <  long.length; i++) {
        for(var j = 0; j < short.length; j++){
           if(short[j] !== long[i + j]) {
-              console.log("BREAK!");
               break;
           }
           if(j === short.length - 1){
-              console.log('FOUND ONE');
               count++;
           }
        }
