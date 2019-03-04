6d3dfa549d8e82be88f2227f97009bec5beb6fe5
Ravi Kant <ravi.kant@magicsw.com>
2019-03-04 02:28:45 -0800

    Improve code

--- SearchingAlogrithm/BinarySearch.js
+++ SearchingAlogrithm/BinarySearch.js
@@ -3,18 +3,11 @@ function binarySearch (arr, elem) {
     let end = arr.length - 1;
     let middle = Math.floor((end + start) / 2);
     while(arr[middle] !== elem && start <= end){
-        if(elem < arr[middle]){
-            end = middle - 1;
-        }else{
-            start = middle + 1;
-        }
+        if(elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
         middle = Math.floor((start + end) / 2);
     }
-    console.log(start, middle, end);
-    if(arr[middle] === elem){
-        return middle;
-    }
-    return -1;
+    return arr[middle] === elem ? middle : -1;
 }
 
-binarySearch([1,2,3,5,6,7,8,9],50);
+binarySearch([1,2,3,5,6,7,8,9],5);
